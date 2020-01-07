f9002aa0f5034ca24eab234f50738f4d4c46c26a
Victor L. Senior <publicdomainvic@gmail.com>
2020-01-07 11:03:31 -0400

    Add SettingsData class

---
+++ MultilingualWordCounter/SettingsData.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="SettingsData.cs" company="PublicDomain.com">
+//     CC0 1.0 Universal (CC0 1.0) - Public Domain Dedication
+//     https://creativecommons.org/publicdomain/zero/1.0/legalcode
+// </copyright>
+
+namespace MultilingualWordCounter
+{
+    // Directives
+    using System;
+
+    /// <summary>
+    /// Settings data.
+    /// </summary>
+    public class SettingsData
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:MultilingualWordCounter.SettingsData"/> class.
+        /// </summary>
+        public SettingsData()
+        {
+            // Parameterless constructor for serialization to work
+        }
+
+        /// <summary>
+        /// Gets or sets the native language.
+        /// </summary>
+        /// <value>The native language.</value>
+        public string NativeLanguage { get; set; } = "English";
+
+        /// <summary>
+        /// Gets or sets the foreign language.
+        /// </summary>
+        /// <value>The foreign language.</value>
+        public string ForeignLanguage { get; set; } = "Italian";
+
+        /// <summary>
+        /// Gets or sets the speech speed.
+        /// </summary>
+        /// <value>The speech speed.</value>
+        public string SpeechSpeed { get; set; } = "Average";
+    }
+}
