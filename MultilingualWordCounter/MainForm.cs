d4a18348358f69f0354e7c53d792f25b91948686
Victor L. Senior <publicdomainvic@gmail.com>
2020-01-24 23:30:32 -0400

    Update RestoreFromSystemTray() function

--- MultilingualWordCounter/MainForm.cs
+++ MultilingualWordCounter/MainForm.cs
@@ -17,6 +17,7 @@ namespace MultilingualWordCounter
     using System.Windows.Forms;
     using System.Xml.Serialization;
     using Microsoft.Win32;
+    using PublicDomain;
 
     /// <summary>
     /// Main form.
@@ -208,6 +209,8 @@ namespace MultilingualWordCounter
                         // Show program window
                         this.RestoreFromSystemTray();
 
+                        // 
+
                         // Halt flow
                         break;
 
@@ -297,7 +300,7 @@ namespace MultilingualWordCounter
         /// <param name="wordsPerMinute">Words per minute.</param>
         private TimeSpan GetSpeechTimeSpan(string text, string language, int wordsPerMinute)
         {
-            // Return the timespan
+            // Return the speech timespan
             return TimeSpan.FromSeconds((this.CountWords(text) * 60) / wordsPerMinute);
         }
 
@@ -309,7 +312,7 @@ namespace MultilingualWordCounter
         private string TimeSpanToHumanReadable(TimeSpan timeSpan)
         {
             // Check for zero length
-            if (timeSpan.TotalSeconds == 0)
+            if (TimeSpan.Compare(timeSpan, TimeSpan.Zero) == 0)
             {
                 // Halt flow
                 return "No words to process";
@@ -437,7 +440,48 @@ namespace MultilingualWordCounter
         /// <param name="e">Event arguments.</param>
         private void OnAboutToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Set license text
+            var licenseText = $"CC0 1.0 Universal (CC0 1.0) - Public Domain Dedication{Environment.NewLine}" +
+                $"https://creativecommons.org/publicdomain/zero/1.0/legalcode{Environment.NewLine}{Environment.NewLine}" +
+                $"Libraries and icons have separate licenses.{Environment.NewLine}{Environment.NewLine}" +
+                $"Speed indicator icon by Openclipart - Pixabay License{Environment.NewLine}" +
+                $"https://pixabay.com/vectors/counter-speed-indicator-speedometer-162094/{Environment.NewLine}{Environment.NewLine}" +
+                $"Minimize icon by made by Gregor Cresnar from www.flaticon.com{Environment.NewLine}" +
+                $"https://www.flaticon.com/authors/gregor-cresnar{Environment.NewLine}{Environment.NewLine}" +
+                $"Patreon icon used according to published brand guidelines{Environment.NewLine}" +
+                $"https://www.patreon.com/brand{Environment.NewLine}{Environment.NewLine}" +
+                $"GitHub mark icon used according to published logos and usage guidelines{Environment.NewLine}" +
+                $"https://github.com/logos{Environment.NewLine}{Environment.NewLine}" +
+                $"DonationCoder icon used with permission{Environment.NewLine}" +
+                $"https://www.donationcoder.com/forum/index.php?topic=48718{Environment.NewLine}{Environment.NewLine}" +
+                $"PublicDomain icon is based on the following source images:{Environment.NewLine}{Environment.NewLine}" +
+                $"Bitcoin by GDJ - Pixabay License{Environment.NewLine}" +
+                $"https://pixabay.com/vectors/bitcoin-digital-currency-4130319/{Environment.NewLine}{Environment.NewLine}" +
+                $"Letter P by ArtsyBee - Pixabay License{Environment.NewLine}" +
+                $"https://pixabay.com/illustrations/p-glamour-gold-lights-2790632/{Environment.NewLine}{Environment.NewLine}" +
+                $"Letter D by ArtsyBee - Pixabay License{Environment.NewLine}" +
+                $"https://pixabay.com/illustrations/d-glamour-gold-lights-2790573/{Environment.NewLine}{Environment.NewLine}";
+
+            // Set about form
+            var aboutForm = new AboutForm(
+                $"About {this.friendlyName}",
+                $"{this.friendlyName} {this.semanticVersion}",
+                $"Made for: Stephen66515{Environment.NewLine}DonationCoder.com{Environment.NewLine}Week #03 @ January 2020",
+                licenseText,
+                this.Icon.ToBitmap());
+
+            // Check for an associated icon
+            if (this.associatedIcon == null)
+            {
+                // Set associated icon from exe file, once
+                this.associatedIcon = Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location);
+            }
+
+            // Set about form icon
+            aboutForm.Icon = this.associatedIcon;
+
+            // Show about form
+            aboutForm.ShowDialog();
         }
 
         /// <summary>
@@ -527,6 +571,9 @@ namespace MultilingualWordCounter
 
             // Return window back to normal
             this.WindowState = FormWindowState.Normal;
+
+            // Bring to the front of the Z-order
+            this.BringToFront();
 
             // Hide system tray icon
             this.mainNotifyIcon.Visible = false;
